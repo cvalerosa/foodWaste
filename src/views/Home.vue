b922015c19c48b12ae5cbc4c9f90cb2d2bbddf0f
Carolina Vale Rosa <carolinavalerosa@gmail.com>
2020-10-11 11:30:58 +0200

    Figma live

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,13 +10,13 @@ import Figma from "vue-low-code";
 Vue.use(Figma);
 
 // import JSON file for deployment
-// import app from './app.json'
+import app from "./app.json";
 
 export default {
   name: "Home",
   data: function() {
     return {
-      // figmaJSON: app,
+      figmaJSON: app,
       figmaConfig: {
         figmaFile: "ine8bnxFRsTfxskewHSAv8",
         figmaAccessKey: "68678-d96bffc3-79a4-4ea2-89d6-47ef2c7f48ac",

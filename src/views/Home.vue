838859293854ed65bf1a46d01b56a2b6c606042d
Carolina Vale Rosa <carolinavalerosa@gmail.com>
2020-10-11 10:55:29 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <Figma :figma="figmaConfig" v-model="viewModel" />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Figma from "vue-low-code";
+Vue.use(Figma);
+
+// import JSON file for deployment
+// import app from './app.json'
+
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      // figmaJSON: app,
+      figmaConfig: {
+        figmaFile: "ine8bnxFRsTfxskewHSAv8",
+        figmaAccessKey: "68678-d96bffc3-79a4-4ea2-89d6-47ef2c7f48ac",
+      },
+      viewModel: {
+        /**
+         * Add your view model here
+         */
+      },
+      config: {
+        components: {
+          /**
+           * Register costum components here
+           */
+        },
+      },
+    };
+  },
+  components: {},
+  methods: {
+    /**
+     * Place your methods here
+     */
+  },
+};
+</script>
